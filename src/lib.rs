8d221d1c1d2eefb845c3d3f822dce165d48add94
Martin Habovštiak <martin.habovstiak@gmail.com>
2017-07-06 22:06:10 +0200

    Added dp_assert and documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,41 @@
+//! This crate provides macros that look just like `panic!()` but instead of panicking, they cause
+//! linking error if their calls are not optimized-out. This can be used to ensure the compiler
+//! optimizes away some code.
+//!
+//! # Example
+//!
+//! ```no_compile
+//! #[macro_use]
+//! extern crate dont_panic;
+//!
+//! fn main() {
+//! /*
+//!     let x = 6 * 9;
+//!     if x == 42 {
+//!         dont_panic!("6 * 9 == 42");
+//!     }
+//! */
+//! let x = false;
+//! if x {
+//!     dont_panic!("42");
+//! }
+//! }
+//! ```
+//!
+//! Compile with `--release` or `--features=panic`
+
 #![no_std]
 
 extern "C" {
+    /// This function doesn't actually exist. It ensures linking error if it isn't optimized-out.
     pub fn rust_panic_called_where_shouldnt() -> !;
 }
 
+/// This macro doesn't panic. Instead it tries to call non-existing function. If the compiler can
+/// prove it can't be called an optimizes it away, the code will compile just fine. Else you get
+/// linking error.
+///
+/// This should be used only in cases you are absolutely sure are OK and optimizable by compiler.
 #[cfg(not(feature = "panic"))]
 #[macro_export]
 macro_rules! dont_panic {
@@ -18,6 +50,8 @@ macro_rules! dont_panic {
     });
 }
 
+/// This macro is active only with `panic` feature turned on and it will really panic, instead of
+/// linking error. The purpose is to make development easier. (E.g. in debug mode.)
 #[cfg(feature = "panic")]
 #[macro_export]
 macro_rules! dont_panic {
@@ -30,6 +64,22 @@ macro_rules! dont_panic {
     ($fmt:expr, $($arg:tt)+) => ({
         panic!($fmt, $($arg)+);
     });
+}
+
+/// Like asser but calls `dont_panic!()` instead of `panic!()`
+#[macro_export]
+macro_rules! dp_assert {
+    ($cond:expr) => (
+        if !$cond {
+            dont_panic!(concat!("assertion failed: ", stringify!($cond)))
+        }
+    );
+
+    ($cond:expr, $($arg:tt)+) => (
+        if !$cond {
+            dont_panic!($($arg)+)
+        }
+    );
 }
 
 #[cfg(test)]
